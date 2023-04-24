746ffdfd6ccfbf591ec27c73291ac06de0039689
yastrb <uaalena202@gmail.com>
2023-04-24 12:45:44 +0300

    new commit

--- js/app.js
+++ js/app.js
@@ -1,3 +1,11 @@
 ﻿window.addEventListener("scroll", e => {
-    document.body.style.cssText=`--scrollTop: ${this.scrollY}px`
+    document.body.style.cssText +=`--scrollTop: ${this.scrollY}px`
 })
+
+gsap.registerPlugin(ScrollTrigger, ScrollSmoother)
+ScrollSmoother.create(
+{
+    wrapper: "wrapper",
+    content: "content"
+}
+)
